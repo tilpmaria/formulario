8b9f3b0d0e652d45b4592a6fe34a4ecae0230a6d
tilpmaria <tilp.maria@escola.pr.gov.br>
2025-09-15 11:24:28 -0300

    .

--- script.js
+++ script.js
@@ -4,5 +4,19 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
 
 function mostraPalavrasChave() { 
-  alert("fui clicado!"); 
-} 
+let palavras = texto.split(/\P{L}+/u);
+
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras);
+
+    const frequencias = contaFrequencias(palavras);
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    return ordenadas.slice(0, 10);
+}
